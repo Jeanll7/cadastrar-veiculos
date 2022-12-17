465a45f4d67142700003ab53fc3867a7a5d55cea
devjean <jeancarloleall@gmail.com>
2022-12-17 10:29:12 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, InputGroup, Form, Table } from "react-bootstrap";
+import { useState } from "react";
+import { BsFillTrashFill } from "react-icons/bs";
 
 function App() {
+  const [placa, setPlaca] = useState("");
+  const [modelo, setModelo] = useState("");
+  const [marca, setMarca] = useState("");
+  const [ano, setAno] = useState("");
+  const [veiculos, setVeiculos] = useState([]);
+
+  function cadastrar() {
+    let validoParaCadastro = true;
+
+    veiculos.forEach((veiculo) => {
+      if (veiculo.placa === placa) {
+        validoParaCadastro = false;
+      }
+    });
+
+    if (validoParaCadastro) {
+      let veiculo = {
+        placa: placa,
+        modelo: modelo,
+        marca,
+        ano,
+      };
+      setVeiculos([veiculo, ...veiculos]);
+      alert("Veiculo cdastrado com sucesso!");
+      limparForm();
+    }
+  }
+
+  function excluir(placa) {
+    veiculos.forEach((veiculo, index) => {
+      if (veiculo.placa === placa) {
+        veiculos.splice(index, 1);
+        setVeiculos([...veiculos]);
+        alert("Veiculo excluido com sucesso");
+      }
+    });
+  }
+
+  function limparForm() {
+    setPlaca("");
+    setModelo("");
+    setMarca("");
+    setAno("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <InputGroup className="mb-2 mt-5">
+          <Form.Control
+            value={placa}
+            onChange={(e) => {
+              setPlaca(e.target.value);
+            }}
+            placeholder="Placa"
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+        <InputGroup className="mb-2">
+          <Form.Control
+            value={modelo}
+            onChange={(e) => {
+              setModelo(e.target.value);
+            }}
+            placeholder="Modelo"
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+        <InputGroup className="mb-2">
+          <Form.Control
+            value={marca}
+            onChange={(e) => {
+              setMarca(e.target.value);
+            }}
+            placeholder="Marca"
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+        <InputGroup className="mb-3">
+          <Form.Control
+            value={ano}
+            onChange={(e) => {
+              setAno(e.target.value);
+            }}
+            placeholder="Ano"
+            aria-label="Default"
+            aria-describedby="inputGroup-sizing-default"
+          />
+        </InputGroup>
+        <Button onClick={cadastrar} className="mb-3">
+          Salvar
+        </Button>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Placa</th>
+              <th>Modelo</th>
+              <th>Marca</th>
+              <th>Ano</th>
+              <th>Acão</th>
+            </tr>
+          </thead>
+          <tbody>
+            {veiculos.map((veiculo) => {
+              return (
+                <tr>
+                  <td>{veiculo.placa}</td>
+                  <td>{veiculo.modelo}</td>
+                  <td>{veiculo.marca}</td>
+                  <td>{veiculo.ano}</td>
+                  <td>
+                    <Button
+                      onClick={() => {
+                        excluir(veiculo.placa);
+                      }}
+                    >
+                      <BsFillTrashFill />
+                    </Button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+    </>
   );
 }
 
